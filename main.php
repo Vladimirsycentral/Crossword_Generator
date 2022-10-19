6c339b890e57b88963a586a3c6fa70093762c49d
The_yarik_guy <48446036+Vladimirsycentral@users.noreply.github.com>
2022-10-19 19:42:52 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,39 @@
+<?php
+$filename = __DIR__ . '/text.txt';
+$array = file($filename);
+$N = 5;
+$used = array(true);
+$CurrentWord = $array[0];
+
+for ($i = 0; $i < $N-1; $i++){
+    array_push($used, false);
+}
+DFS(1, $N, $array, $CurrentWord);
+echo ("\n");
+for ($i = 0; $i < $N; $i++){
+    if ($used[$i] == false){
+        echo 0;
+        return;
+    }
+}
+echo 1;
+
+function DFS($i, $N, $array, $CurrentWord)
+{
+    $flag= false;
+    global $used;
+    $used[$i] = true;
+    for ($l = 1; $l < $N; $l++){
+        for ($k = 1; $k < strlen($array[$l])-1; $k++){
+            for ($g = 1; $g < strlen($CurrentWord)-1; $g++){
+                $checkingWord = $array[$l];
+                if (($CurrentWord[$g] == $checkingWord[$k]) && ($flag == false) && ($used[$l]==false)){
+                    $flag = true;
+                    $CurrentWord = $checkingWord;
+                    DFS($l, $N, $array, $CurrentWord);
+                }
+            }
+        }
+    }
+}
+
